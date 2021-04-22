254f33d90b5a7f6db514efa055a3997f539dc024
Lengyue <zhonglifei@hotmail.com>
2021-04-22 16:46:59 +0800

    add test.py

---
+++ test.py
@@ -0,0 +1,93 @@
+import os
+import random
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.autograd import Variable
+import torch.nn.functional as F
+import numpy as np
+import time
+import math
+from math import exp
+from dataloader import MiddleburyDataset
+from models import MVMEFNet
+from utils import batch_PSNR, batch_mse
+import cv2
+import PIL
+
+torch.manual_seed(0)
+torch.cuda.manual_seed(0)
+
+l = [
+    # 'Adirondack-perfect',
+    # 'Backpack-perfect',
+    'Bicycle1-perfect',
+    # 'Cable-perfect',
+    'Classroom1-perfect',
+    # 'Couch-perfect',
+    # 'Flowers-perfect',
+    # 'Jadeplant-perfect',
+    # 'Mask-perfect',
+    # 'Motorcycle-perfect',
+    'Piano-perfect',
+    # 'Pipes-perfect',
+    # 'Playroom-perfect',
+    # 'Playtable-perfect',
+    # 'Recycle-perfect',
+    # 'Shelves-perfect',
+    # 'Shopvac-perfect',
+    # 'Sticks-perfect',
+    # 'Storage-perfect',
+    # 'Sword1-perfect',
+    # 'Sword2-perfect',
+    # 'Umbrella-perfect',
+    # 'Vintage-perfect'
+ ]
+
+BATCH_SIZE = 1
+LR = 1e-3
+
+test_dataset = MiddleburyDataset("~/disk/middlebury", l,
+                            crop_height=768, crop_width=1408)
+
+test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=BATCH_SIZE, shuffle=True, num_workers=1)
+
+model = MVMEFNet()
+model = model.cuda()
+
+save_num = 1100
+model_path = './savepoints/%d.pkl'%save_num
+if os.path.exists(model_path):
+    model_dict = torch.load(model_path)
+    model.load_state_dict(model_dict)
+    print('loading dict')
+
+model.eval()
+
+for step, sample in enumerate(test_loader):
+    left, right, left_g, right_g, left_o, right_o, right_gt, disp = \
+        sample['left'], sample['right'], sample['left_g'], sample['right_g'], sample['left_o'], sample['right_o'], sample['right_gt'], sample['left_disparity']
+
+    PIL.Image.fromarray(np.uint8(left_o.numpy()[0].transpose(1,2,0)*255)).save('./test_out/%d_left_o.png'%step)
+    PIL.Image.fromarray(np.uint8(right_o.numpy()[0].transpose(1,2,0)*255)).save('./test_out/%d_right_o.png'%step)
+    PIL.Image.fromarray(np.uint8(right_gt.numpy()[0].transpose(1,2,0)*255)).save('./test_out/%d_right_gt.png'%step)
+    PIL.Image.fromarray(np.uint8(disp.numpy()[0])).save('./test_out/%d_disp.png'%step)
+
+    left, right, left_g, right_g, left_o, right_o, right_gt, disp = left.cuda(), right.cuda(), left_g.cuda(), right_g.cuda(), left_o.cuda(), right_o.cuda(), right_gt.cuda(), disp.cuda()
+
+    with torch.no_grad():
+        pred1, pred2, pred3, w_imgL_o, result = model(left, right, left_g, right_g, left_o, right_o)
+    
+
+    PIL.Image.fromarray(np.uint8(w_imgL_o.cpu().numpy()[0].transpose(1,2,0)*255)).save('./test_out/%d_w_imgL_o.png'%step)
+    PIL.Image.fromarray(np.uint8(result.cpu().numpy()[0].transpose(1,2,0)*255)).save('./test_out/%d_result.png'%step)
+    PIL.Image.fromarray(np.uint8(pred3.cpu().numpy()[0])).save('./test_out/%d_pred.png'%step)
+
+    mask = (disp < 192) & (disp > 0)
+
+    PSNR = batch_PSNR(torch.clamp(result, 0., 1.), right_gt, 1.)
+    MSE = batch_mse(pred3[mask], disp[mask])
+
+    print("[step %d][PSNR : %7f][MSE : %7f]" % (step, PSNR, MSE))
+
+    
