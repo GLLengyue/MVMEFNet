31a7b062fd7dcd2d94323eeac2daa91c14644d5e
Lengyue <zhonglifei@hotmail.com>
2021-05-04 21:29:41 +0800

    change test img

--- test_img.py
+++ test_img.py
@@ -10,6 +10,7 @@ from models import MVMEFNet
 from utils import batch_PSNR, batch_me
 from PIL import Image
 import cv2
+import torchvision.transforms as transforms
 
 def get_transform():
     mean = [0.485, 0.456, 0.406]
@@ -19,7 +20,8 @@ def get_transform():
         transforms.ToTensor(),
         transforms.Normalize(mean=mean, std=std),
     ])
-    
+
+processed = get_transform()
 torch.manual_seed(0)
 torch.cuda.manual_seed(0)
 
@@ -37,7 +39,11 @@ if os.path.exists(model_path):
 model.eval()
 
 left_img = Image.open('./imgL.png').convert('RGB')
+left_img = left_img.crop((0,0,4096,2048))
+left_img = left_img.resize((left_img.size[0]//4, left_img.size[1]//4))
 right_img = Image.open('./imgR.png').convert('RGB')
+right_img = right_img.crop((0,0,4096,2048))
+right_img = right_img.resize((right_img.size[0]//4, right_img.size[1]//4))
 
 left_img = np.asarray(left_img, dtype='float32')/255
 right_img = np.asarray(right_img, dtype='float32')/255
@@ -62,10 +68,18 @@ left, right, left_g, right_g, left_o, right_o = \
 
 left, right, left_g, right_g, left_o, right_o = left.cuda(), right.cuda(), left_g.cuda(), right_g.cuda(), left_o.cuda(), right_o.cuda()
 
+left = torch.unsqueeze(left, 0)
+right = torch.unsqueeze(right, 0)
+left_g = torch.unsqueeze(left_g, 0)
+right_g = torch.unsqueeze(right_g, 0)
+left_o = torch.unsqueeze(left_o, 0)
+right_o = torch.unsqueeze(right_o, 0)
+
+
 with torch.no_grad():
     pred1, pred2, pred3, w_imgL_o, result, a_map = model(left, right, left_g, right_g, left_o, right_o)
 
-PIL.Image.fromarray(np.uint8(w_imgL_o.cpu().numpy()[0].transpose(1,2,0)*255)).save('./test_out/%d_w_imgL_o.png'%step)
-PIL.Image.fromarray(np.uint8(torch.clamp(result, 0., 1.).cpu().numpy()[0].transpose(1,2,0)*255)).save('./test_out/%d_result.png'%step)
-PIL.Image.fromarray(np.uint8(pred3.cpu().numpy()[0])).save('./test_out/%d_pred.png'%step)
-PIL.Image.fromarray(np.uint8(255*np.mean(a_map.cpu().numpy()[0], axis=0))).save('./test_out/%d_map.png'%step)
+Image.fromarray(np.uint8(w_imgL_o.cpu().numpy()[0].transpose(1,2,0)*255)).save('./w_imgL_o.png')
+Image.fromarray(np.uint8(torch.clamp(result, 0., 1.).cpu().numpy()[0].transpose(1,2,0)*255)).save('./result.png')
+Image.fromarray(np.uint8(pred3.cpu().numpy()[0])).save('./pred.png')
+Image.fromarray(np.uint8(255*np.mean(a_map.cpu().numpy()[0], axis=0))).save('./test_out/map.png')
