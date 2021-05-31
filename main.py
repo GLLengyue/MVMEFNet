3d4434e50171b543b56fbed4d4582fa9ba7ee5f3
Lengyue <zhonglifei@hotmail.com>
2021-05-31 16:51:02 +0800

    5_31 add refine module

--- main.py
+++ main.py
@@ -97,16 +97,18 @@ for epoch in range(save_num+1, 10000):
         optimizer.zero_grad()
 
         # Generate a batch of images
-        pred1, pred2, pred3, w_imgL_o, result, a_map = model(left, right, left_g, right_g, left_o, right_o)
+        pred1, pred2, pred3, w_imgL_o, result, a_map, result_r = model(left, right, left_g, right_g, left_o, right_o)
 
         # calculate PSNR
         PSNR = batch_PSNR(torch.clamp(result, 0., 1.), right_gt, 1.)
+        PSNR_r = batch_PSNR(torch.clamp(result_r, 0., 1.), right_gt, 1.)
 
         # calculate disp ME
         ME = batch_me(pred3[mask], disp[mask])
 
         # loss aggregation
         g_loss = L1loss(result, right_gt)
+        g_loss += L1loss(result_r, right_gt)
         g_loss += L1loss(pred3[mask], disp[mask])
         g_loss += L1loss(w_imgL_o[:,:,:,:CROP_WIDTH//2], warped_gt[:,:,:,:CROP_WIDTH//2])
         # t1 = torch.square(w_imgL_o-warped_gt)
@@ -114,20 +116,22 @@ for epoch in range(save_num+1, 10000):
         # g_loss += -torch.mean(a_map)
         # g_loss += torch.mean(t1*t2)
 
-        if PSNR < 100:
+        if PSNR < 100 and PSNR_r < 100:
             e_loss += g_loss.item()
             e_PSNR += PSNR
+            e_PSNR_r += PSNR_r
             e_ME += ME
             count+=1
-        print("\r[Epoch %d][Loss: %7f][PSNR : %7f][ME : %7f]" % (epoch, g_loss.item(), PSNR, ME), end='')
+        print("\r[Epoch %d][Loss: %7f][PSNR : %7f][PSNR_r : %7f][ME : %7f]" % (epoch, g_loss.item(), PSNR, PSNR_r, ME), end='')
 
         g_loss.backward()
         optimizer.step()
     e_loss = e_loss/count
     e_PSNR = e_PSNR/count
+    e_PSNR_r = e_PSNR_r/count
     e_ME = e_ME/count
-    print("\r[Epoch %d][Loss: %7f][PSNR : %7f][ME : %7f]" % (epoch, e_loss, e_PSNR, e_ME), end='\n')
+    print("\r[Epoch %d][Loss: %7f][PSNR : %7f][PSNR_r : %7f][ME : %7f]" % (epoch, e_loss, e_PSNR, e_PSNR_r, e_ME), end='\n')
     with open('losses.txt', 'a') as f:
-        f.write("\r[Epoch %d][Loss: %7f][PSNR : %7f][ME : %7f]" % (epoch, e_loss, e_PSNR, e_ME))
+        f.write("\r[Epoch %d][Loss: %7f][PSNR : %7f][PSNR_r : %7f][ME : %7f]" % (epoch, e_loss, e_PSNR, e_PSNR_r,e_ME))
     if (epoch % 100 == 0):
         torch.save(model.state_dict(), './savepoints/%d.pkl' % (epoch))
